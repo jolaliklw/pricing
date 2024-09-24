32219ac7f4bf4a4d0ddff25caa4aa348b7f52fe2
jolaliklw <jolalikaliwungu@gmail.com>
2024-09-24 08:25:27 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 // We listen to the resize event
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
 // ====================
 //
 // ====================
 // get data from local storage
-const dataStored = JSON.parse(window.localStorage.getItem('data'));
+const dataStored = JSON.parse(window.localStorage.getItem("data"));
 
 if (!dataStored) {
-  const data = { platform: '.', hpr: '', ppn: 0, extra: 0 };
-  window.localStorage.setItem('data', JSON.stringify(data));
+  const data = { platform: ".", hpr: "", ppn: 0, extra: 0 };
+  window.localStorage.setItem("data", JSON.stringify(data));
   location.reload();
 }
 // localStorage.clear();
 
 if (dataStored) {
   document
-    .querySelector('input#harga')
-    .setAttribute('value', numberWithDot(dataStored.hpr.toString()));
+    .querySelector("input#harga")
+    .setAttribute("value", numberWithDot(dataStored.hpr.toString()));
 
-  document.querySelector('#set-ppn').setAttribute('value', dataStored.ppn);
+  document.querySelector("#set-ppn").setAttribute("value", dataStored.ppn);
 
-  document.querySelector('#set-extra').setAttribute('value', dataStored.extra);
+  document.querySelector("#set-extra").setAttribute("value", dataStored.extra);
 }
 
-const hargaBaru = document.querySelector('input#harga');
-const berat = document.querySelector('input#berat');
-const inputs = document.querySelectorAll('input');
-const showHarga = document.querySelector('#show-harga .nominal');
-const shortInfo = document.querySelector('.short-info p');
+const hargaBaru = document.querySelector("input#harga");
+const berat = document.querySelector("input#berat");
+const inputs = document.querySelectorAll("input");
+const showHarga = document.querySelector("#show-harga .nominal");
+const shortInfo = document.querySelector(".short-info p");
 
 function numberWithDot(x) {
-  x = x.replace(/[^,\d]/g, '').toString();
+  x = x.replace(/[^,\d]/g, "").toString();
   let pattern = /(-?\d+)(\d{3})/;
-  while (pattern.test(x)) x = x.replace(pattern, '$1.$2');
+  while (pattern.test(x)) x = x.replace(pattern, "$1.$2");
 
   return x;
 }
 
 const rupiah = (number) => {
-  return new Intl.NumberFormat('id-ID', {
+  return new Intl.NumberFormat("id-ID", {
     // style: 'currency',
-    currency: 'IDR',
+    currency: "IDR",
     minimumFractionDigits: 0,
   }).format(number);
 };
 
 const totalHarga = () => {
   // replace . to emty
-  const hargaPerGram = hargaBaru.value.replace(/[\s.]/g, '');
+  const hargaPerGram = hargaBaru.value.replace(/[\s.]/g, "");
   // pembulatan angka
   const hargaAkhir = Math.round(
     hargaPerGram * berat.value * (dataStored.ppn / 100 + 1) +
@@ -77,7 +77,7 @@ inputs.forEach((input) => {
   input.addEventListener(`focus`, () => input.select());
 
   input.oninput = (e) => {
-    if (e.target.id === 'harga') {
+    if (e.target.id === "harga") {
       e.target.value = numberWithDot(e.target.value);
     }
 
@@ -85,16 +85,16 @@ inputs.forEach((input) => {
   };
 
   // click enter to save
-  input.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-      if (event.target.id == 'berat') {
+  input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      if (event.target.id == "berat") {
         const addDecimals = Number(event.target.value);
         event.target.value = addDecimals.toFixed(2);
 
         // save input harga baru when enter
         if (numberWithDot(dataStored.hpr.toString()) !== hargaBaru.value) {
           dataStored.hpr = hargaBaru.value;
-          window.localStorage.setItem('data', JSON.stringify(dataStored));
+          window.localStorage.setItem("data", JSON.stringify(dataStored));
         }
       }
 
@@ -104,12 +104,12 @@ inputs.forEach((input) => {
 });
 
 // click anywhere to save
-window.addEventListener('click', (e) => {
+window.addEventListener("click", (e) => {
   e.preventDefault();
 
   // click anywhere kecuali ini
-  if (e.target.id === 'berat' || e.target.id === 'harga') {
-    e.target.addEventListener('click', (e) => {
+  if (e.target.id === "berat" || e.target.id === "harga") {
+    e.target.addEventListener("click", (e) => {
       e.stopPropagation();
     });
   }
@@ -123,51 +123,51 @@ window.addEventListener('click', (e) => {
   if (dataStored) {
     if (numberWithDot(dataStored.hpr.toString()) !== hargaBaru.value) {
       dataStored.hpr = hargaBaru.value;
-      window.localStorage.setItem('data', JSON.stringify(dataStored));
+      window.localStorage.setItem("data", JSON.stringify(dataStored));
     }
   }
 });
 
 // format 1000 to 1k
 const formatNumber = (number) => {
-  number = number.toLocaleString('en-US', {
+  number = number.toLocaleString("en-US", {
     maximumFractionDigits: 2,
-    notation: 'compact',
-    compactDisplay: 'short',
+    notation: "compact",
+    compactDisplay: "short",
   });
 
   return number;
 };
 
 if (!dataStored) {
-  shortInfo.innerHTML = 'data not found.';
+  shortInfo.innerHTML = "data not found.";
 } else {
   shortInfo.innerHTML = `${dataStored.platform} / ${formatNumber(
-    dataStored.ppn == 0 ? '-' : dataStored.ppn
+    dataStored.ppn == 0 ? "-" : dataStored.ppn
   )} / ${
-    dataStored.extra == 0 ? '-' : formatNumber(Number(dataStored.extra))
-  } ${'\u00A9' + new Date().getFullYear()}`;
+    dataStored.extra == 0 ? "-" : formatNumber(Number(dataStored.extra))
+  } ${"\u00A9" + new Date().getFullYear()}`;
 }
 
 // modal
-const modal = document.querySelector('.setting-modal');
-const btnOpenModal = document.querySelector('.btn-open-modal');
-const btnCloseModal = document.querySelector('.btn-close-modal');
-const btnSave = document.querySelector('.btn-save');
-const setPpn = document.querySelector('#set-ppn');
-const setExtra = document.querySelector('#set-extra');
+const modal = document.querySelector(".setting-modal");
+const btnOpenModal = document.querySelector(".btn-open-modal");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnSave = document.querySelector(".btn-save");
+const setPpn = document.querySelector("#set-ppn");
+const setExtra = document.querySelector("#set-extra");
 
-btnOpenModal.addEventListener('click', (e) => {
+btnOpenModal.addEventListener("click", (e) => {
   e.preventDefault();
-  modal.style.display = 'block';
+  modal.style.display = "block";
 });
 
-btnCloseModal.addEventListener('click', (e) => {
+btnCloseModal.addEventListener("click", (e) => {
   e.preventDefault();
-  modal.style.display = 'none';
+  modal.style.display = "none";
 });
 
-btnSave.addEventListener('click', (e) => {
+btnSave.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (setPpn.value.length == 0) {
@@ -176,10 +176,10 @@ btnSave.addEventListener('click', (e) => {
   if (setExtra.value.length == 0) {
     dataStored.extra = 0;
   }
-  dataStored.platform = '-';
+  dataStored.platform = "-";
   dataStored.ppn = setPpn.value;
   dataStored.extra = setExtra.value;
-  window.localStorage.setItem('data', JSON.stringify(dataStored));
+  window.localStorage.setItem("data", JSON.stringify(dataStored));
   location.reload();
-  modal.style.display = 'none';
+  modal.style.display = "none";
 });
